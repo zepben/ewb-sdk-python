2011b6050156b0320f71a9b0ea0af215d7d23904
kurt <kurt.greaves@zepben.com>
2019-11-15 15:17:23 +1100

    Add test files (no tests yet)

---
+++ test/test_model.py
@@ -0,0 +1,7 @@
+
+"""
+TODO:
+    test usagePoint/Meter conversion of equipment to mRIDs and reversed.
+    test Meter supporting mRIDs or equipment references
+    test ordering of send for UsagePoints, Meters, and MeterReadings
+"""
