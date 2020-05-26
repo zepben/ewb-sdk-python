27f66fa73317500bc61ba928166dc846a1c7582a
Clyde Uyenghua <clyde.uyenghua@zepben.com>
2020-05-26 05:41:23 +0000

    bitbucket pipeline config. [skip ci]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_namespace_packages
 test_deps = ["pytest", "pytest-asyncio"]
 setup(
     name="zepben.cimbend",
-    version="0.2.0",
+    version="0.2.0b1",
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
     install_requires=[
