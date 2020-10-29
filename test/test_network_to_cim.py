1b75b9d7c9e6079a7a4c4c83efa7017c2c1f6364
kgreav <kurt.greaves@zepben.com>
2020-10-29 12:13:13 +1100

    Setup build, deployment, and release workflows (#1)
    * Setup build, deployment, and release workflows
    
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- test/test_network_to_cim.py
+++ test/test_network_to_cim.py
@@ -1,4 +1,8 @@
-
+#  Copyright 2020 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from zepben.protobuf.cim.iec61970.base.core.IdentifiedObject_pb2 import IdentifiedObject as PBIdentifiedObject
 from zepben.protobuf.cim.iec61968.assetinfo.CableInfo_pb2 import CableInfo as PBCableInfo
