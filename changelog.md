a1533b52797aa1c8c4406e25776206014e65ae7d
kgreav <kurt.greaves@zepben.com>
2020-12-17 17:38:38 +1100

    Ewb 373 add measurement producer (#7)
    * Add MeasurementProducerClient and corresponding functionality
    
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- changelog.md
+++ changelog.md
@@ -1,10 +1,11 @@
 ### v0.21.0
 
 ##### Breaking Changes
-* None.
+* Re-ordered the internal modules. If you were previously importing from something other than the top-level module (zepben.evolve), 
+  it's likely these imports will no longer work. You should change all your imports to only import from zepben.evolve.
 
 ##### New Features
-* None.
+* Added MeasurementProducerClient for streaming measurements. Currently experimental and API will definitely change.
 
 ##### Enhancements
 * None.
--- src/zepben/evolve/services/measurement/measurements.py
+++ src/zepben/evolve/services/measurement/measurements.py
@@ -3,6 +3,8 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from __future__ import annotations
+from typing import List, Optional, Generator
 
 from zepben.evolve.services.common.base_service import BaseService
 
@@ -11,3 +13,17 @@ __all__ = ["MeasurementService"]
 
 class MeasurementService(BaseService):
     name: str = "measurement"
+    _measurements: List[MeasurementValue] = []
+
+    def add(self, value: MeasurementValue):
+        self._measurements.append(value)
+
+    def remove(self, value: MeasurementValue):
+        self._measurements.remove(value)
+
+    def len_of(self, t: type = None) -> int:
+        return len([m for m in self._measurements if isinstance(m, t)]) if t is not None else len(self._measurements)
+
+    def objects(self, obj_type: Optional[type] = None, exc_types: Optional[List[type]] = None) -> Generator[IdentifiedObject, None, None]:
+        for m in self._measurements:
+            yield m
