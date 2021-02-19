b7fb33c1fd0cc6cf3be1fec428bc370375d1fe30
Carlos <carlos.macana@zepben.com>
2021-02-19 15:02:46 +1100

    Adding: License to network_creator.py & simple_bus_branch.py
    Signed-off-by: Carlos <carlos.macana@zepben.com>

--- src/zepben/evolve/examples/networks/simple_bus_branch.py
+++ src/zepben/evolve/examples/networks/simple_bus_branch.py
@@ -1,3 +1,13 @@
+#  Copyright 2020 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from collections import defaultdict
 
 from zepben.evolve import NetworkService, DiagramService, Diagram, \
