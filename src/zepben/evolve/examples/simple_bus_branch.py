8ad6fe078817885c30f1b1c049242295b08ec38c
Kurt Greaves <kurt.greaves@zepben.com>
2021-02-22 09:49:56 +1100

    Reorganise files
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- src/zepben/evolve/examples/simple_bus_branch.py
+++ src/zepben/evolve/examples/simple_bus_branch.py
@@ -3,16 +3,12 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#
-#  This Source Code Form is subject to the terms of the Mozilla Public
-#  License, v. 2.0. If a copy of the MPL was not distributed with this
-#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from collections import defaultdict
 
 from zepben.evolve import NetworkService, DiagramService, Diagram, \
     DiagramStyle, BaseVoltage, PositionPoint, Location, Feeder, EnergySource, PowerTransformerInfo, DiagramObject, \
-    AcLineSegment, DiagramObjectStyle, IdentifiedObject, ConductingEquipment, Junction, EnergyConsumer, \
+    AcLineSegment, DiagramObjectStyle, ConductingEquipment, Junction, EnergyConsumer, \
     PowerTransformer, DiagramObjectPoint
 
 
