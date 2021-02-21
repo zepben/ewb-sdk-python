a415aa40c60096006416912b62c343fe22850f9b
Kurt Greaves <kurt.greaves@zepben.com>
2021-02-22 09:17:42 +1100

    Add new DER classes: PowerElectronicsConnection, PowerElectronicsConnectionPhase, PowerElectronicsUnit, BatteryUnit, PhotoVoltaicUnit, PowerElectronicsWindUnit and enumeration BatteryStateKind
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

---
+++ src/zepben/evolve/model/cim/iec61970/base/wires/generation/production/battery_state_kind.py
@@ -0,0 +1,34 @@
+#  Copyright 2021 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from enum import Enum
+
+__all__ = ["BatteryStateKind"]
+
+
+class BatteryStateKind(Enum):
+    """Battery state"""
+    
+    UNKNOWN = 0
+    """Battery state is not known."""
+
+    DISCHARGING = 1
+    """Stored energy is decreasing."""
+
+    FULL = 2
+    """Unable to charge, and not discharging."""
+
+    WAITING = 3
+    """Neither charging nor discharging, but able to do so."""
+
+    CHARGING = 4
+    """Stored energy is increasing."""
+
+    EMPTY = 5
+    """Unable to discharge, and not charging."""
+
+    @property
+    def short_name(self):
+        return str(self)[17:]
