44920f3c08415bb06eb71bec5bea234d2281658b
Kurt Greaves <kurt.greaves@zepben.com>
2021-02-22 15:34:16 +1100

    Update BusbarSection tests with new property based testing framework
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- test/cim/iec61970/base/wires/test_connectors.py
+++ test/cim/iec61970/base/wires/test_connectors.py
@@ -3,10 +3,19 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from hypothesis import given
+
+from test.cim.constructor_validation import verify_connector_constructor, cn_kwargs, cn_args, verify_cn_args
 from zepben.evolve import BusbarSection
 
 
-def test_constructor():
+bbs_kwargs = cn_kwargs
+bbs_args = cn_args
+
+
+@given(**bbs_kwargs)
+def test_busbarsection_constructor_kwargs(**kwargs):
+    verify_connector_constructor(BusbarSection, **kwargs)
     bbs = BusbarSection()
     assert bbs.mrid
     bbs = BusbarSection(mrid="test")
@@ -15,3 +24,6 @@ def test_constructor():
     assert bbs.mrid == "test2"
 
 
+def test_busbarsection_constructor_args():
+    bbs = BusbarSection(*bbs_args)
+    verify_cn_args(bbs)
