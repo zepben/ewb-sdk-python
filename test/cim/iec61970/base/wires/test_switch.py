a90443f6d60abfd38e3eca3101080d837b5b48a4
Carlos <carlos.macana@zepben.com>
2021-02-24 10:26:18 +1100

    Update license for test_switch.py
    Signed-off-by: Carlos <carlos.macana@zepben.com>

--- test/cim/iec61970/base/wires/test_switch.py
+++ test/cim/iec61970/base/wires/test_switch.py
@@ -1,4 +1,11 @@
+#  Copyright 2021 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from hypothesis import given
+
 
 from cim.constructor_validation import ps_kwargs, ps_args, verify_connector_constructor, \
     verify_protected_switch_constructor, verify_ps_args
