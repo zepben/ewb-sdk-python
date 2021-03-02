4e03390f35cb854fe3f05ae7ee9ede25741d799d
cmacana <54392395+cmacana@users.noreply.github.com>
2021-03-02 17:47:50 +1100

    EWB-589_Fix-the-simple_bus_branchpy (#27)
    * Change simple_bus_branch to simple_node_breaker_feeder and use ConnectivityNodes
    
    Signed-off-by: Carlos <carlos.macana@zepben.com>

--- src/zepben/evolve/services/network/network_extensions.py
+++ src/zepben/evolve/services/network/network_extensions.py
@@ -17,27 +17,30 @@ from zepben.evolve.model.cim.iec61970.base.wires.energy_source import EnergySour
 from zepben.evolve.model.cim.iec61970.base.wires.power_transformer import PowerTransformer, PowerTransformerEnd
 from zepben.evolve.model.cim.iec61970.base.wires.aclinesegment import AcLineSegment
 from zepben.evolve.model.cim.iec61970.base.wires.energy_consumer import EnergyConsumer
+from zepben.evolve.model.cim.iec61970.base.wires.switch import Breaker
+from zepben.evolve.model.cim.iec61970.base.core.connectivity_node import ConnectivityNode
 from zepben.evolve.util import CopyableUUID
 
-__all__ = ["create_ac_line_segment", "create_two_winding_power_transformer", "create_energy_consumer", "create_energy_source", "create_bus"]
+__all__ = ["create_ac_line_segment", "create_two_winding_power_transformer", "create_energy_consumer",
+           "create_energy_source", "create_bus", "create_breaker"]
 
 
 # !! WARNING !! #
 # THIS CODE IS IN ACTIVE DEVELOPMENT, UNSTABLE, AND LIKELY TO HAVE ISSUES. FOR EXPERIMENTATION ONLY.
 
-def create_ac_line_segment(network_service: NetworkService, bus1: Junction, bus2: Junction,
+def create_ac_line_segment(network_service: NetworkService, cn1: ConnectivityNode, cn2: ConnectivityNode,
                            **kwargs) -> AcLineSegment:
     acls = AcLineSegment(**kwargs)
     _create_two_terminal_conducting_equipment(network_service=network_service, ce=acls)
-    _connect_two_terminal_conducting_equipment(network_service=network_service, ce=acls, bus1=bus1, bus2=bus2)
+    _connect_two_terminal_conducting_equipment(network_service=network_service, ce=acls, cn1=cn1, cn2=cn2)
     return acls
 
 
-def create_two_winding_power_transformer(network_service: NetworkService, bus1: Junction, bus2: Junction,
+def create_two_winding_power_transformer(network_service: NetworkService, cn1: ConnectivityNode, cn2: ConnectivityNode,
                                          **kwargs) -> PowerTransformer:
     power_transformer = PowerTransformer(**kwargs)
     _create_two_terminal_conducting_equipment(network_service=network_service, ce=power_transformer, **kwargs)
-    _connect_two_terminal_conducting_equipment(network_service=network_service, ce=power_transformer, bus1=bus1, bus2=bus2)
+    _connect_two_terminal_conducting_equipment(network_service=network_service, ce=power_transformer, cn1=cn1, cn2=cn2)
     # TODO: How to associated PowerTransformerEndInfo to a PowerTransformerInfo
     for i in range(1, 2):
         end = PowerTransformerEnd(power_transformer=power_transformer)
@@ -46,18 +49,25 @@ def create_two_winding_power_transformer(network_service: NetworkService, bus1: 
     return power_transformer
 
 
-def create_energy_consumer(net: NetworkService, bus: Junction, **kwargs) -> EnergyConsumer:
+def create_energy_consumer(net: NetworkService, cn: ConnectivityNode, **kwargs) -> EnergyConsumer:
     ec = EnergyConsumer(**kwargs)
     _create_single_terminal_conducting_equipment(network_service=net, ce=ec, **kwargs)
-    _connect_single_terminal_conducting_equipment(network_service=net, ce=ec, bus=bus)
+    _connect_single_terminal_conducting_equipment(network_service=net, ce=ec, cn=cn)
     return ec
 
 
-def create_energy_source(net: NetworkService, bus: Junction, **kwargs) -> EnergySource:
+def create_energy_source(net: NetworkService, cn: ConnectivityNode, **kwargs) -> EnergySource:
     es = EnergySource(**kwargs)
-    _create_single_terminal_conducting_equipment(network_service=net, ce=es, bus=bus, **kwargs)
-    _connect_single_terminal_conducting_equipment(network_service=net, ce=es, bus=bus)
+    _create_single_terminal_conducting_equipment(network_service=net, ce=es, cn=cn, **kwargs)
+    _connect_single_terminal_conducting_equipment(network_service=net, ce=es, cn=cn)
     return es
+
+
+def create_breaker(network_service: NetworkService, cn1: ConnectivityNode, cn2: ConnectivityNode, **kwargs) -> Breaker:
+    ce = Breaker(**kwargs)
+    _create_two_terminal_conducting_equipment(network_service=network_service, ce=ce, **kwargs)
+    _connect_two_terminal_conducting_equipment(network_service=network_service, ce=ce, cn1=cn1, cn2=cn2)
+    return ce
 
 
 def create_bus(network_service: NetworkService, **kwargs) -> Junction:
@@ -78,9 +88,10 @@ def _create_two_terminal_conducting_equipment(network_service: NetworkService, c
     _create_terminals(ce=ce, num_terms=2, network=network_service)
 
 
-def _connect_two_terminal_conducting_equipment(network_service: NetworkService, ce: ConductingEquipment, bus1: Junction, bus2: Junction):
-    network_service.connect_terminals(bus1.get_terminal_by_sn(1), ce.get_terminal_by_sn(1))
-    network_service.connect_terminals(bus2.get_terminal_by_sn(1), ce.get_terminal_by_sn(2))
+def _connect_two_terminal_conducting_equipment(network_service: NetworkService, ce: ConductingEquipment,
+                                               cn1: ConnectivityNode, cn2: ConnectivityNode):
+    network_service.connect_by_mrid(ce.get_terminal_by_sn(1), cn1.mrid)
+    network_service.connect_by_mrid(ce.get_terminal_by_sn(2), cn2.mrid)
 
 
 def _create_single_terminal_conducting_equipment(network_service: NetworkService, ce: ConductingEquipment, **kwargs):
@@ -90,11 +101,13 @@ def _create_single_terminal_conducting_equipment(network_service: NetworkService
     _create_terminals(ce=ce, network=network_service)
 
 
-def _connect_single_terminal_conducting_equipment(network_service: NetworkService, ce: ConductingEquipment, bus: Junction):
-    network_service.connect_terminals(ce.get_terminal_by_sn(1), bus.get_terminal_by_sn(1))
+def _connect_single_terminal_conducting_equipment(network_service: NetworkService, ce: ConductingEquipment,
+                                                  cn: ConnectivityNode):
+    network_service.connect_by_mrid(ce.get_terminal_by_sn(1), cn.mrid)
 
 
-def _create_terminals(network: NetworkService, ce: ConductingEquipment, num_terms: int = 1, phases: PhaseCode = PhaseCode.ABC):
+def _create_terminals(network: NetworkService, ce: ConductingEquipment, num_terms: int = 1,
+                      phases: PhaseCode = PhaseCode.ABC):
     for i in range(1, num_terms + 1):
         terminal: Terminal = Terminal(mrid=f"{ce.mrid}_t{i}", conducting_equipment=ce, phases=phases, sequence_number=i)
         ce.add_terminal(terminal)
@@ -107,3 +120,4 @@ NetworkService.create_energy_source = create_energy_source
 NetworkService.create_two_winding_power_transformer = create_two_winding_power_transformer
 NetworkService.create_ac_line_segment = create_ac_line_segment
 NetworkService.create_energy_consumer = create_energy_consumer
+NetworkService.create_breaker = create_breaker
--- test/services/network/test_network_extensions.py
+++ test/services/network/test_network_extensions.py
@@ -3,65 +3,52 @@ from collections import namedtuple
 from pytest import fixture
 
 from zepben.evolve import NetworkService, Junction, BaseVoltage, Terminal, EnergySource, \
-    PowerTransformer, AcLineSegment, EnergyConsumer, PowerTransformerInfo, PositionPoint, Location
+    PowerTransformer, AcLineSegment, EnergyConsumer, PowerTransformerInfo, PositionPoint, Location, \
+    ConnectivityNode, Breaker
 
-TestNetworkCreator = namedtuple("TestNetworkCreator", ["net", "bv", "bus1", "bus2", "pt_info", "loc1", "loc2", "loc3"])
+TestNetworkCreator = namedtuple("TestNetworkCreator", ["net", "bv", "cn1", "cn2", "pt_info", "loc1", "loc2", "loc3"])
 
 
 @fixture()
 def tnc():
     net = NetworkService()
     bv = BaseVoltage()
-    bus1 = net.create_bus(base_voltage=bv)
-    bus2 = net.create_bus(base_voltage=bv)
+    cn1 = ConnectivityNode()
+    cn2 = ConnectivityNode()
     pt_info = PowerTransformerInfo()
     point1 = PositionPoint(x_position=149.12791965570293, y_position=-35.277592101000934)
     point2 = PositionPoint(x_position=149.12779472660375, y_position=-35.278183862759285)
     loc1 = Location().add_point(point1)
     loc2 = Location().add_point(point2)
     loc3 = Location().add_point(point2).add_point(point2)
-    yield TestNetworkCreator(net=net, bv=bv, bus1=bus1, bus2=bus2, pt_info=pt_info, loc1=loc1, loc2=loc2, loc3=loc3)
-
-
-def test_create_bus(tnc):
-    tnc.net.create_bus(base_voltage=tnc.bv)
-    objects = tnc.net.objects(Junction)
-    for ce in objects:
-        bus: Junction = ce
-        assert isinstance(bus, Junction)
-        assert bus.num_terminals() == 1, "num_terminals should be 1"
-        t: Terminal = bus.get_terminal_by_sn(1)
-        assert t is not None
-        assert bus.get_base_voltage() is tnc.bv, f'bus.get_base_voltage() is not {tnc.bv}. Instead is {bus.get_base_voltage()}'
-        assert t.conducting_equipment is bus, "t.conducting_equipment should be 'bus'"
+    yield TestNetworkCreator(net=net, bv=bv, cn1=cn1, cn2=cn2, pt_info=pt_info, loc1=loc1, loc2=loc2, loc3=loc3)
 
 
 def test_create_energy_source(tnc):
-    tnc.net.create_energy_source(bus=tnc.bus1)
-    objects = tnc.net.objects(EnergySource)
-    for source in objects:
-        t: Terminal = source.get_terminal_by_sn(1)
-        assert source.num_terminals() == 1
-        assert isinstance(t, Terminal)
-        assert isinstance(source, EnergySource)
-        assert t.conducting_equipment is source, "t.conducting_equipment should be 'source'"
+    ce1 = tnc.net.create_energy_source(cn=tnc.cn1)
+    ce2 = tnc.net.get(ce1.mrid)
+    assert ce1 is ce2
+    assert isinstance(ce1, EnergySource)
+    t: Terminal = ce1.get_terminal_by_sn(1)
+    assert ce1.num_terminals() == 1
+    assert isinstance(t, Terminal)
+    assert t.conducting_equipment is ce1, "t.conducting_equipment should be 'source'"
 
 
 def test_create_energy_consumer(tnc):
-    tnc.net.create_energy_consumer(bus=tnc.bus1, location=tnc.loc1)
-    objects = tnc.net.objects(EnergyConsumer)
-    for ce in objects:
-        consumer: EnergyConsumer = ce
-        t: Terminal = consumer.get_terminal_by_sn(1)
-        assert consumer.num_terminals() == 1
-        assert isinstance(t, Terminal)
-        assert isinstance(consumer, EnergyConsumer)
-        assert t.conducting_equipment is consumer, "t.conducting_equipment should be 'ec'"
-        assert consumer.location == tnc.loc1
+    ce1 = tnc.net.create_energy_consumer(cn=tnc.cn1, location=tnc.loc1)
+    ce2 = tnc.net.get(ce1.mrid)
+    assert ce1 is ce2
+    assert ce2.num_terminals() == 1
+    t: Terminal = ce2.get_terminal_by_sn(1)
+    assert isinstance(t, Terminal)
+    assert isinstance(ce2, EnergyConsumer)
+    assert t.conducting_equipment is ce2, "t.conducting_equipment should be 'ce1'"
+    assert ce2.location == tnc.loc1
 
 
 def test_create_two_winding_power_transformer(tnc):
-    tnc.net.create_two_winding_power_transformer(bus1=tnc.bus1, bus2=tnc.bus2, asset_info=tnc.pt_info,
+    tnc.net.create_two_winding_power_transformer(cn1=tnc.cn1, cn2=tnc.cn2, asset_info=tnc.pt_info,
                                                  location=tnc.loc1)
     objects = tnc.net.objects(PowerTransformer)
     for ce in objects:
@@ -75,13 +62,27 @@ def test_create_two_winding_power_transformer(tnc):
 
 
 def test_create_ac_line_segment(tnc):
-    tnc.net.create_ac_line_segment(bus1=tnc.bus1, bus2=tnc.bus2, location=tnc.loc3)
-    objects = tnc.net.objects(AcLineSegment)
-    for ce in objects:
-        line_segment: AcLineSegment = ce
-        t: Terminal = line_segment.get_terminal_by_sn(1)
-        assert line_segment.num_terminals() == 2
-        assert isinstance(t, Terminal)
-        assert isinstance(line_segment, AcLineSegment)
-        assert t.conducting_equipment is line_segment, "t.conducting_equipment should be 'line_segment'"
-        assert line_segment.location == tnc.loc3
+    ce1 = tnc.net.create_ac_line_segment(cn1=tnc.cn1, cn2=tnc.cn2, location=tnc.loc3)
+    ce2 = tnc.net.get(ce1.mrid)
+    assert ce1 is ce2
+    t: Terminal = ce1.get_terminal_by_sn(1)
+    assert ce1.num_terminals() == 2
+    assert isinstance(t, Terminal)
+    assert isinstance(ce1, AcLineSegment)
+    assert t.conducting_equipment is ce1, "t.conducting_equipment should be 'line_segment'"
+    assert ce1.location == tnc.loc3
+
+
+def test_create_breaker(tnc):
+    b1 = tnc.net.create_breaker(cn1=tnc.cn1, cn2=tnc.cn2, location=tnc.loc3)
+    b2 = tnc.net.get(b1.mrid)
+    assert b1 is b2
+    assert isinstance(b1, Breaker)
+    assert b1.num_terminals() == 2
+    t1: Terminal = b1.get_terminal_by_sn(1)
+    t2: Terminal = b1.get_terminal_by_sn(2)
+    assert isinstance(t1, Terminal)
+    assert isinstance(t2, Terminal)
+    assert t1.conducting_equipment is b1, "t.conducting_equipment should be 'line_segment'"
+    assert t1.conducting_equipment is b1, "t.conducting_equipment should be 'line_segment'"
+    assert b1.location == tnc.loc3
