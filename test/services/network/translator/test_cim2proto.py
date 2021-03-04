528bd370584f842b8e5e2c92464f1dfba8f04b42
Abdullah <abdullah.elahi@zepben.com>
2021-03-04 17:56:50 +1100

    initial serialisation/deserialisation testing framework developed
    Signed-off-by: Abdullah <abdullah.elahi@zepben.com>

--- test/services/network/translator/test_cim2proto.py
+++ test/services/network/translator/test_cim2proto.py
@@ -6,9 +6,21 @@
 from hypothesis import given
 from zepben.protobuf.cim.iec61970.base.wires.BusbarSection_pb2 import BusbarSection
 from zepben.protobuf.cim.iec61970.base.wires.LoadBreakSwitch_pb2 import LoadBreakSwitch
+from zepben.protobuf.cim.iec61970.base.wires.Junction_pb2 import Junction
+from zepben.protobuf.cim.iec61970.base.wires.AcLineSegment_pb2 import AcLineSegment
+from zepben.protobuf.cim.iec61970.base.wires.EnergySource_pb2 import EnergySource
+from zepben.protobuf.cim.iec61970.base.wires.EnergyConsumer_pb2 import EnergyConsumer
+from zepben.protobuf.cim.iec61970.base.wires.Disconnector_pb2 import Disconnector
+from zepben.protobuf.cim.iec61970.base.wires.Fuse_pb2 import Fuse
+from zepben.protobuf.cim.iec61970.base.wires.Jumper_pb2 import Jumper
+from zepben.protobuf.cim.iec61970.base.wires.Breaker_pb2 import Breaker
+from zepben.protobuf.cim.iec61970.base.wires.LinearShuntCompensator_pb2 import LinearShuntCompensator
+from zepben.protobuf.cim.iec61970.base.wires.RatioTapChanger_pb2 import RatioTapChanger
 
-from test.cim_creators import busbarsection
-from test.cim_creators import loadbreakswitch
+
+
+from test.cim_creators import busbarsection, loadbreakswitch, energysource, energyconsumer, junction, aclinesegment, \
+disconnector, fuse, jumper, breaker, linearshuntcompensator, ratiotapchanger
 
 
 @given(bbs=busbarsection())
@@ -17,6 +29,29 @@ def test_busbar_to_pb(bbs):
     assert pb.mrid() == bbs.mrid
     assert isinstance(pb, BusbarSection)
 
+@given(dis=disconnector())
+def test_disconnector_to_pb(dis):
+    pb = dis.to_pb()
+    assert pb.mrid() == dis.mrid
+    assert isinstance(pb, Disconnector)
+
+@given(fus=fuse())
+def test_fuse_to_pb(fus):
+    pb = fus.to_pb()
+    assert pb.mrid() == fus.mrid
+    assert isinstance(pb, Fuse)
+
+@given(jum=jumper())
+def test_jumper_to_pb(jum):
+    pb = jum.to_pb()
+    assert pb.mrid() == jum.mrid
+    assert isinstance(pb, Jumper)
+
+@given(brk=breaker())
+def test_breaker_to_pb(brk):
+    pb = brk.to_pb()
+    assert pb.mrid() == brk.mrid
+    assert isinstance(pb, Breaker)
 
 @given(lbs=loadbreakswitch())
 def test_loadbreakswitch_to_pb(lbs):
@@ -42,3 +77,53 @@ def verify_switch(cim, pb):
         assert pb.ps.sw.open
     if cim.is_normally_open():
         assert pb.ps.sw.normalOpen
+
+@given(jnc=junction())
+def test_junction_to_pb(jnc):
+    pb = jnc.to_pb()
+    assert pb.mrid() == jnc.mrid
+    assert isinstance(pb, Junction)
+
+@given(acl=aclinesegment())
+def test_aclinesegment_to_pb(acl):
+    pb = acl.to_pb()
+    assert pb.mrid() == acl.mrid
+    assert isinstance(pb, AcLineSegment)
+
+@given(ens=energysource())
+def test_energysource_to_pb(ens):
+    pb = ens.to_pb()
+    assert pb.mrid() == ens.mrid
+    assert isinstance(pb, EnergySource)
+    assert pb.activePower == ens.active_power
+    assert pb.r == ens.r
+    assert pb.x == ens.x
+    assert pb.reactivePower == ens.reactive_power
+    assert pb.voltageAngle == ens.voltage_angle
+    assert pb.voltageMagnitude == ens.voltage_magnitude
+    assert pb.pMax == ens.p_max
+    assert pb.pMin == ens.p_min
+    assert pb.r0 == ens.r0
+    assert pb.rn == ens.rn
+    assert pb.x0 == ens.x0
+    assert pb.xn == ens.xn
+
+
+@given(enc=energyconsumer())
+def test_energyconsumer_to_pb(enc):
+    pb = enc.to_pb()
+    assert pb.mrid() == enc.mrid
+    assert isinstance(pb, EnergyConsumer)
+
+def verify_energyconsumer_parameters():
+    pb = enc.to_pb(NetworkService())
+    assert pb.p == enc.p
+    assert pb.q == enc.q
+    assert pb.p_fixed == enc.p_fixed
+    assert pb.q_fixed == enc.q_fixed
+
+#@given(lsc=linearshuntcompensator())
+#def test_linearshuntcompensator_to_pb(lsc):
+    #pb = lsc.to_pb()
+    #assert pb.mrid() == lsc.mrid
+    #assert isinstance(pb, LinearShuntCompensator)'''
--- test/services/network/translator/test_proto2cim.py
+++ test/services/network/translator/test_proto2cim.py
@@ -4,9 +4,11 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from hypothesis import given
-from zepben.evolve import NetworkService, BusbarSection
+from zepben.evolve import NetworkService, BusbarSection, Junction, EnergySource, EnergyConsumer, AcLineSegment, Disconnector, \
+Fuse, Jumper, Breaker, LoadBreakSwitch, PowerTransformer, LinearShuntCompensator, RatioTapChanger
 
-from test.pb_creators import busbarsection
+from test.pb_creators import busbarsection, junction, energysource, energyconsumer, aclinesegment, disconnector, fuse, \
+jumper, breaker, loadbreakswitch, powertransformer, linearshuntcompensator, ratiotapchanger
 
 
 @given(bbs=busbarsection())
@@ -14,3 +16,95 @@ def test_busbar_to_cim(bbs):
     cim = bbs.to_cim(NetworkService())
     assert cim.mrid == bbs.mrid()
     assert isinstance(cim, BusbarSection)
+
+@given(jnc=junction())
+def test_junction_to_cim(jnc):
+    cim = jnc.to_cim(NetworkService())
+    assert cim.mrid == jnc.mrid()
+    assert isinstance(cim, Junction)
+
+@given(acl=aclinesegment())
+def test_aclinesegment_to_cim(acl):
+    cim = acl.to_cim(NetworkService())
+    assert cim.mrid == acl.mrid()
+    assert isinstance(cim, AcLineSegment)
+
+@given(ens=energysource())
+def test_energysource_to_cim(ens):
+    cim = ens.to_cim(NetworkService())
+    assert cim.mrid == ens.mrid()
+    assert isinstance(cim, EnergySource)
+    assert cim.active_power == ens.activePower
+    assert cim.r == ens.r
+    assert cim.x == ens.x
+    assert cim.reactive_power == ens.reactivePower
+    assert cim.voltage_angle == ens.voltageAngle
+    assert cim.voltage_magnitude == ens.voltageMagnitude
+    assert cim.p_max == ens.pMax
+    assert cim.p_min == ens.pMin
+    assert cim.r0 == ens.r0
+    assert cim.rn == ens.rn
+    assert cim.x0 == ens.x0
+    assert cim.xn == ens.xn
+
+@given(enc=energyconsumer())
+def test_energyconsumer_to_cim(enc):
+    cim = enc.to_cim(NetworkService())
+    assert cim.mrid == enc.mrid()
+    assert isinstance(cim, EnergyConsumer)
+
+def verify_energyconsumer_parameters():
+    cim = enc.to_cim(NetworkService())
+    assert cim.p == enc.p
+    assert cim.q == enc.q
+    assert cim.p_fixed == enc.p_fixed
+    assert cim.q_fixed == enc.q_fixed
+
+@given(dis=disconnector())
+def test_disconnector_to_cim(dis):
+    cim = dis.to_cim(NetworkService())
+    assert cim.mrid == dis.mrid()
+    assert isinstance(cim, Disconnector)
+
+@given(fus=fuse())
+def test_fuse_to_cim(fus):
+    cim = fus.to_cim(NetworkService())
+    assert cim.mrid == fus.mrid()
+    assert isinstance(cim, Fuse)
+
+@given(jum=jumper())
+def test_jumper_to_cim(jum):
+    cim = jum.to_cim(NetworkService())
+    assert cim.mrid == jum.mrid()
+    assert isinstance(cim, Jumper)
+
+@given(brk=breaker())
+def test_breaker_to_cim(brk):
+    cim = brk.to_cim(NetworkService())
+    assert cim.mrid == brk.mrid()
+    assert isinstance(cim, Breaker)
+
+@given(lbs=loadbreakswitch())
+def test_loadbreakswitch_to_cim(lbs):
+    cim = lbs.to_cim(NetworkService())
+    assert cim.mrid == lbs.mrid()
+    assert isinstance(cim, LoadBreakSwitch)
+
+@given(pwt=powertransformer())
+def test_disconnector_to_cim(pwt):
+    cim = pwt.to_cim(NetworkService())
+    assert cim.mrid == pwt.mrid()
+    assert isinstance(cim, PowerTransformer)
+
+
+#@given(rtc=ratiotapchanger())
+#def test_ratiotapchanger_to_cim(rtc):
+    #cim = rtc.to_cim(NetworkService())
+    #assert cim.mrid == rtc.mrid()
+    #assert isinstance(cim, RatioTapChanger)
+
+#@given(lsc=LinearShuntCompensator())
+#def test_linearshuntcompensator_to_cim(lsc):
+    #cim = lsc.to_cim(NetworkService())
+    #assert cim.mrid == lsc.mrid()
+    #assert isinstance(cim, LinearShuntCompensator)'''
