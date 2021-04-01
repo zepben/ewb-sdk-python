888092f497949d5055bb2f230a09b9538e63f33b
Abdullah <abdullah.elahi@zepben.com>
2021-04-01 17:12:48 +1100

    initial implementation of Names package in Python SDK
    Signed-off-by: Abdullah <abdullah.elahi@zepben.com>

--- src/zepben/evolve/model/cim/iec61970/base/core/name_type.py
+++ src/zepben/evolve/model/cim/iec61970/base/core/name_type.py
@@ -1,11 +1,14 @@
-#  Copyright 2020 Zeppelin Bend Pty Ltd
+#  Copyright 2021 Zeppelin Bend Pty Ltd
 #
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+from __future__ import annotations
 
-class nameType(object):
+from zepben.evolve.model.cim.iec61970.base.core.identified_object import IdentifiedObject
+
+class nameType(IdentifiedObject):
     """
     Type of name. Possible values for attribute 'name' are implementation dependent but standard profiles may specify types. An enterprise may have multiple
     IT systems each having its own local name for the same object, e.g. a planning system may have different names from an EMS. An object may also have
@@ -14,3 +17,4 @@ class nameType(object):
     A power system related naming hierarchy may be: Substation, VoltageLevel, Equipment etc. Children of the same parent in such a hierarchy have names that
     typically are unique among them.
     """
+
