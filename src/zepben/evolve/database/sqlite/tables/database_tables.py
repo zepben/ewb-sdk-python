f256c6d9aac3d0a36dd3a1dc7e77e57617b11217
Kurt Greaves <kurt.greaves@zepben.com>
2021-04-16 11:54:54 +1000

    Duplicate licence header
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- src/zepben/evolve/database/sqlite/tables/database_tables.py
+++ src/zepben/evolve/database/sqlite/tables/database_tables.py
@@ -3,10 +3,7 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#
-#  This Source Code Form is subject to the terms of the Mozilla Public
-#  License, v. 2.0. If a copy of the MPL was not distributed with this
-#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from typing import Dict, TypeVar, Type
 
 from dataclassy import dataclass
