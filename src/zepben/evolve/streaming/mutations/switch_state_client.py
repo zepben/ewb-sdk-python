e946699af359375253ff4b3894c9f21689ab5103
Abdullah <abdullah.elahi@zepben.com>
2021-06-01 16:43:42 +1000

    switch state stuff
    Signed-off-by: Abdullah <abdullah.elahi@zepben.com>

--- src/zepben/evolve/streaming/mutations/switch_state_client.py
+++ src/zepben/evolve/streaming/mutations/switch_state_client.py
@@ -4,19 +4,55 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from grpc import Channel
+from typing import List, Callable
+from zepben.protobuf.nm.set_switch_state_data_pb2 import SwitchStateUpdate as PBSwitchStateUpdate
+from zepben.protobuf.nm.set_switch_state_requests_pb2 import SetCurrentSwitchStatesRequest
 from zepben.evolve.streaming.grpc.grpc import GrpcClient
-from zepben.evolve.streaming.grpc.grpc import GrpcResult
-from
-from zepben.evolve.streaming.mutations.switch_state_update import SwitchStateUpdate as PBSwitchStateUpdate
+from zepben.protobuf.nm.set_switch_state_pb2_grpc import SwitchStateServiceStub
+from zepben.evolve.streaming.mutations.switch_state_update import SwitchStateUpdate
 
-stub: SwitchStateServiceGrpc.SwitchStateServiceBlocking
+class SwitchStateClient(GrpcClient):
 
-class SwitchStateClient(object):
-    GrpcClient()
+    _stub: SwitchStateServiceStub = None
 
-    constructor(channel: Channel): self.SwitchStateServiceGrpc.newBlockingStub(channel))
+    def __init__(self, channel=None, stub: SwitchStateServiceStub = None, error_handlers: List[Callable[[Exception], bool]] = None):
+        super().__init__(error_handlers=error_handlers)
+        if channel is None and stub is None:
+            raise ValueError("Must provide either a channel or a stub")
+        if stub is not None:
+            self._stub = stub
+        else:
+            self._stub = SwitchStateServiceStub(channel)
 
+    def set_current_switch_state(self, PBSwitchStateUpdate):
+        """
+        Send a request to the server to update the current state of a switch.
+        switchToUpdate -> The switch and its state to be updated.
+        return - GrpcResult that indicates success or failure of the remote call.
+        """
+
+        self.set_current_switch_states(list(PBSwitchStateUpdate))
+
+
+    def set_current_switch_states(self,switches_to_update: List[PBSwitchStateUpdate]):
+        """
+        Send a request to the server to update the current state of a group of switches as a batch.
+        All switches included in the request will be treated as a batch. That is, the resulting network state will
+        only be calculated once all the new switch states have been applied. This can then avoid
+        where the model may be in an 'inconsistent' state if you know a set of switch operations should for some reason
+        be applied all together.
+
+        Note: This should not be used to send large amounts of switch updates for unrelated groups of switching,
+        as excessively large state updates can starve consumers / readers getting access to the model.
+        """
+        request = SetCurrentSwitchStatesRequest(switchesToUpdate=[x.to_pb() for x in switches_to_update])
+        return self.try_rpc(lambda: self._stub.setCurrentSwitchStates(request))
+
+
+def switch_state_update_to_pb(cim: SwitchStateUpdate) -> PBSwitchStateUpdate:
+    return PBSwitchStateUpdate(mRID=cim.mrid,
+                               setOpen=cim.set_open,
+                               timestamp=cim.timestamp.timestamp())
 
 
 
--- src/zepben/evolve/streaming/mutations/switch_state_update.py
+++ src/zepben/evolve/streaming/mutations/switch_state_update.py
@@ -4,3 +4,24 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+from datetime import datetime
+from dataclassy import dataclass
+
+
+@dataclass()
+class SwitchStateUpdate:
+    """
+    Class to hold details for setting a new switch state.
+    """
+
+    mrid: str
+    """The MRID of the switch to be updated."""
+
+    set_open: bool
+    """True if the switch should be opened, false if it should be closed."""
+
+    timestamp: datetime = datetime.now()
+    """The time recorded of the actual switch state change occurring. Defaults to now if not provided."""
+
+
+
