bab58deb9723ef7abde9a7f21f2fdb3173eb575d
Anthony Charlton <anthony.charlton@zepben.com>
2021-07-08 11:59:01 +1000

    EWB-745 - Fixed error in hypothesis testing for `IdentifiedObject` names
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- test/cim/iec61970/base/core/test_identified_object.py
+++ test/cim/iec61970/base/core/test_identified_object.py
@@ -7,8 +7,8 @@ import re
 
 from hypothesis.strategies import uuids, text, lists, builds, none
 
-from test.cim.iec61970.base.core.test_name_type import name_type_kwargs
 from test.cim.collection_validator import validate_collection
+from test.cim.iec61970.base.core.test_name_type import name_type_kwargs
 from test.cim_creators import ALPHANUM, TEXT_MAX_SIZE
 from zepben.evolve import IdentifiedObject, Junction
 #
@@ -25,7 +25,9 @@ identified_object_kwargs = {
     "mrid": uuids(version=4).map(lambda x: str(x)),
     "name": text(alphabet=ALPHANUM, max_size=TEXT_MAX_SIZE),
     "description": text(alphabet=ALPHANUM, max_size=TEXT_MAX_SIZE),
-    "names": lists(builds(Name, text(alphabet=ALPHANUM, max_size=TEXT_MAX_SIZE), builds(NameType, **name_type_kwargs), none()), max_size=2)
+    "names": lists(builds(Name, text(alphabet=ALPHANUM, max_size=TEXT_MAX_SIZE), builds(NameType, **name_type_kwargs), none()),
+                   max_size=2,
+                   unique_by=lambda it: it.name)
 }
 
 # noinspection PyArgumentList
