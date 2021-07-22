63fc60b493757dd55cf15657d774db8e84c2d4f6
CI <ci@zepben.com>
2021-07-22 12:11:05 +1000

    Update versions for release [skip ci]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 test_deps = ["pytest", "pytest-cov", "pytest-asyncio", "hypothesis<6"]
 setup(
     name="zepben.evolve",
-    version="0.22.1b1",
+    version="0.22.1",
     description="Python SDK for interacting with the Evolve platform",
     long_description=long_description,
     long_description_content_type="text/markdown",
