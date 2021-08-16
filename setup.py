2084b79431c6639ef9a2570067fb3cf16c0ddb62
Kurt Greaves <kurt.greaves@zepben.com>
2021-08-16 16:12:48 +1000

    Use zepben.auth library for authentication functionality
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "protobuf==3.14.0",
         "requests<2.26.0,>=2.25.0",
         "zepben.protobuf>=0.15.0b10, < 0.16.0",
+        "zepben.auth==0.4.0",
         "PyJWT==2.1.0",
         "dataclassy==0.6.2",
         "grpcio==1.36.0"
