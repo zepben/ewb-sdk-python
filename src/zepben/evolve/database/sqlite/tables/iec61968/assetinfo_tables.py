3209394c1dd25ab552fb69e2b950da5e8904750c
cmacana <carlos.macana@zepben.com>
2021-11-26 20:05:08 +1100

    Adding: Tables for TransformerTests children.
    Signed-off-by: cmacana <carlos.macana@zepben.com>

--- src/zepben/evolve/database/sqlite/tables/iec61968/assetinfo_tables.py
+++ src/zepben/evolve/database/sqlite/tables/iec61968/assetinfo_tables.py
@@ -12,10 +12,107 @@ __all__ = ["TablePowerTransformerInfo", "TableTransformerEndInfo", "TableTransfo
            "TableOverheadWireInfo"]
 
 
+class TableTransformerTest(TableAssetInfo):
+    base_power: Column = None
+    temperature: Column = None
+
+    def __init__(self):
+        super(TableTransformerTest, self).__init__()
+        self.column_index += 1
+        self.base_power = Column(self.column_index, "base_power", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.temperature = Column(self.column_index, "temperature", "NUMBER", Nullable.NULL)
+
+
+class TableNoLoadTests(TableTransformerTest):
+    energized_end_voltage = None
+    exciting_current = None
+    exciting_current_zero = None
+    loss = None
+    loss_zero = None
+
+    def name(self) -> str:
+        return "no_load_tests"
+
+    def __init__(self):
+        super(TableTransformerTest, self).__init__()
+        self.column_index += 1
+        self.energized_end_voltage = Column(self.column_index, "energised_end_voltage", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.exciting_current = Column(self.column_index, "exciting_current", "NUMBER", Nullable.NULL)
+        self.column_index += 1
+        self.exciting_current_zero = Column(self.column_index, "exciting_current_zero", "NUMBER", Nullable.NULL)
+        self.column_index += 1
+        self.loss = Column(self.column_index, "exciting_current", "NUMBER", Nullable.NULL)
+        self.column_index += 1
+        self.loss_zero = Column(self.column_index, "loss_zero", "INTEGER", Nullable.NULL)
+
+
+class TableOpenCircuitTests(TableTransformerTest):
+    energized_end_step = None
+    energized_end_voltage = None
+    open_end_step = None
+    open_end_voltage = None
+    phase_shift = None
+
+    def name(self) -> str:
+        return "open_circuit_tests"
+
+    def __init__(self):
+        super(TableTransformerTest, self).__init__()
+        self.column_index += 1
+        self.energized_end_voltage = Column(self.column_index, "energised_end_step", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.energized_end_step = Column(self.column_index, "energised_end_voltage", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.open_end_step = Column(self.column_index, "open_end_step", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.open_end_voltage = Column(self.column_index, "open_end_voltage", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.phase_shift = Column(self.column_index, "phase_shift", "NUMBER", Nullable.NULL)
+
+
 class TablePowerTransformerInfo(TableAssetInfo):
 
     def name(self) -> str:
-        return "power_transformer_info"
+        return "short_circuit_tests"
+
+
+class TableShortCircuitTests(TableTransformerTest):
+    current = None
+    energized_end_voltage = None
+    grounded_end_step = None
+    leakage_impedance = None
+    leakage_impedance_zero = None
+    loss = None
+    loss_zero = None
+    power = None
+    voltage = None
+    voltage_ohmic_part = None
+
+    def name(self) -> str:
+        return "short_circuit_tests"
+
+    def __init__(self):
+        super(TableTransformerTest, self).__init__()
+        self.column_index += 1
+        self.current = Column(self.column_index, "current", "NUMBER", Nullable.NULL)
+        self.column_index += 1
+        self.energized_end_voltage = Column(self.column_index, "energized_end_voltage", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.grounded_end_step = Column(self.column_index, "grounded_end_step", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.leakage_impedance = Column(self.column_index, "leakage_impedance", "NUMBER", Nullable.NULL)
+        self.column_index += 1
+        self.leakage_impedance_zero = Column(self.column_index, "leakage_impedance_zero", "NUMBER", Nullable.NULL)
+        self.column_index += 1
+        self.loss = Column(self.column_index, "loss", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.loss_zero = Column(self.column_index, "loss_zero", "INTEGER", Nullable.NULL)
+        self.column_index += 1
+        self.voltage = Column(self.column_index, "voltage", "NUMBER", Nullable.NULL)
+        self.column_index += 1
+        self.voltage_ohmic_part = Column(self.column_index, "voltage_ohmic_part", "NUMBER", Nullable.NULL)
 
 
 class TableTransformerEndInfo(TableAssetInfo):
@@ -101,5 +198,3 @@ class TableOverheadWireInfo(TableWireInfo):
 
     def name(self) -> str:
         return "overhead_wire_info"
-
-
