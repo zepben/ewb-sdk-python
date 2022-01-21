d1c7a3fec6f86c5f1372730b29cbe6d64f80822d
Marcus Koh <marcus.koh@zepben.com>
2022-01-21 13:48:00 +1100

    Change table version
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/zepben/evolve/database/sqlite/tables/metadata_tables.py
+++ src/zepben/evolve/database/sqlite/tables/metadata_tables.py
@@ -13,7 +13,7 @@ __all__ = ["TableVersion", "TableMetadataDataSources"]
 class TableVersion(SqliteTable):
     version: Column = None
 
-    SUPPORTED_VERSION = 40
+    SUPPORTED_VERSION = 41
 
     def __init__(self):
         self.version = self._create_column("version", "TEXT", Nullable.NOT_NULL)
