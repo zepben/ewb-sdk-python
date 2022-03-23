4f8fae26ee82ffb42a44368f6f9f41589e2ef1bb
Kurt Greaves <kurt.greaves@zepben.com>
2022-03-23 16:21:07 +1100

    Use latest releases of auth and protobuf
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         "requests<2.27.0,>=2.26.0",
-        "zepben.protobuf==0.19.0b8",
-        "zepben.auth==0.7.0b2",
+        "zepben.protobuf==0.19.0",
+        "zepben.auth==0.8.0",
         "dataclassy==0.6.2",
         "grpcio==1.43.0",
         "six==1.16.0"
