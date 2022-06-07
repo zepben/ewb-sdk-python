07c029453d7f6a9d934a19aaf7e1accf611799f7
Kurt Greaves <kurt.greaves@zepben.com>
2022-06-07 14:02:47 +1000

    Use latest version of zepben.protobuf
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         "requests<2.27.0,>=2.26.0",
-        "zepben.protobuf==0.19.0",
+        "zepben.protobuf==0.20.0",
         "zepben.auth==0.9.0",
         "dataclassy==0.6.2",
         "grpcio==1.43.0",
