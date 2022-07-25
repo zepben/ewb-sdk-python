d48d9f98864612186a57849b44b15821bcdd2795
Marcus Koh <marcus.koh@zepben.com>
2022-07-25 12:45:22 +1000

    Fix another async fixture
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- test/network_fixtures.py
+++ test/network_fixtures.py
@@ -5,6 +5,7 @@
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from typing import Dict, List, Optional
 
+import pytest_asyncio
 from pytest import fixture
 
 from zepben.evolve import NetworkService, Feeder, PhaseCode, EnergySource, EnergySourcePhase, Junction, ConductingEquipment, Breaker, PowerTransformer, \
@@ -288,7 +289,7 @@ def add_location(network: NetworkService, psr: PowerSystemResource, *coords: flo
     network.add(loc)
 
 
-@fixture()
+@pytest_asyncio.fixture()
 async def feeder_network():
     """
                 c1       c2
