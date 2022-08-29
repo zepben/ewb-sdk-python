5693d0e5188d5b38d646adcf573fd71e9e102d3e
CI <ci@zepben.com>
2022-08-29 18:00:33 +1000

    Update versions for release [skip ci]

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ test_deps = [
 
 setup(
     name="zepben.evolve",
-    version="0.32.0b1",
+    version="0.32.0",
     description="Python SDK for interacting with the Evolve platform",
     long_description=long_description,
     long_description_content_type="text/markdown",
