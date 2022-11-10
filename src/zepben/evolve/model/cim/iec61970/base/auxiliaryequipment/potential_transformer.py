bb38d6fedd7065ade304d3e6eb2c030d5d3e8a97
Marcus Koh <marcus.koh@zepben.com>
2022-11-10 20:11:50 +1100

    Revert import order
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/zepben/evolve/model/cim/iec61970/base/auxiliaryequipment/potential_transformer.py
+++ src/zepben/evolve/model/cim/iec61970/base/auxiliaryequipment/potential_transformer.py
@@ -3,6 +3,8 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from __future__ import annotations
+
 from typing import Optional, TYPE_CHECKING
 
 from zepben.evolve.model.cim.iec61970.base.auxiliaryequipment.potential_transformer_kind import PotentialTransformerKind
