3fbf279d0fb28f2077cd26de1f63be18e0e9de05
Marcus Koh <marcus.koh@zepben.com>
2022-11-11 14:31:53 +1100

    Test ratio
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

---
+++ test/cim/iec61968/infiec61968/infcommon/test_ratio.py
@@ -0,0 +1,52 @@
+#  Copyright 2022 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from _pytest.python_api import raises
+from hypothesis import given
+from hypothesis.strategies import floats
+
+from zepben.evolve import Ratio
+
+ratio_kwargs = {
+    "denominator": floats(min_value=0.1, max_value=1000),
+    "numerator": floats(min_value=0.0, max_value=1000)
+}
+
+ratio_args = [9.0, 6.0]
+
+# noinspection PyArgumentList
+def test_ratio_constructor_default():
+    #
+    # NOTE: There is no blank constructor, so check we need to pass both values.
+    #
+    with raises(TypeError):
+        Ratio()
+    with raises(TypeError):
+        Ratio(1.0)
+    with raises(TypeError):
+        Ratio(denominator=2.0)
+    with raises(TypeError):
+        Ratio(numerator=2.0)
+
+
+@given(**ratio_kwargs)
+def test_ratio_constructor_kwargs(denominator, numerator, **kwargs):
+    assert not kwargs
+
+    # noinspection PyArgumentList
+    ratio = Ratio(denominator=denominator, numerator=numerator)
+
+    assert ratio.denominator == denominator
+    assert ratio.numerator == numerator
+
+
+def test_ratio_constructor_args():
+    # noinspection PyArgumentList
+    ratio = Ratio(*ratio_args)
+
+    # non-alphabetic order is due to mathematical convention (numerator before denominator)
+    assert ratio.numerator == ratio_args[-2]
+    assert ratio.denominator == ratio_args[-1]
