9287d57371078307bb08c628c7ce3bdaacb98ca1
Marcus Koh <marcus.koh@zepben.com>
2022-11-11 14:48:46 +1100

    add missing tests
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

---
+++ test/cim/iec61968/infiec61968/infassetinfo/test_transformer_construction_kind.py
@@ -0,0 +1,14 @@
+#  Copyright 2022 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from zepben.protobuf.cim.iec61968.infiec61968.infassetinfo.TransformerConstructionKind_pb2 import TransformerConstructionKind as PBTransformerConstructionKind
+
+from cim.enum_validator import validate_enum
+from zepben.evolve import TransformerConstructionKind
+
+
+def test_transformer_construction_kind_enum():
+    validate_enum(TransformerConstructionKind, PBTransformerConstructionKind)
