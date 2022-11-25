ecb751ec105e1f531bfc756b02abc431088a7fb9
Marcus Koh <marcus.koh@zepben.com>
2022-11-25 11:34:35 +1100

    Test connectivity result
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/zepben/evolve/services/network/tracing/connectivity/connectivity_result.py
+++ src/zepben/evolve/services/network/tracing/connectivity/connectivity_result.py
@@ -7,7 +7,7 @@
 from __future__ import annotations
 
 from operator import attrgetter
-from typing import List, Optional, Tuple, TYPE_CHECKING
+from typing import List, Optional, Tuple, TYPE_CHECKING, Iterable
 
 from dataclassy import dataclass
 
@@ -51,7 +51,7 @@ class ConnectivityResult:
     nominal_phase_paths: Tuple[NominalPhasePath]
     """The mapping of nominal phase paths between the from and to terminals."""
 
-    def __init__(self, from_terminal: Terminal, to_terminal: Terminal, nominal_phase_paths: List[NominalPhasePath]):
+    def __init__(self, from_terminal: Terminal, to_terminal: Terminal, nominal_phase_paths: Iterable[NominalPhasePath]):
         self.nominal_phase_paths = tuple(sorted(nominal_phase_paths, key=attrgetter('from_phase', 'to_phase')))
         self.from_terminal = from_terminal
         self.to_terminal = to_terminal
