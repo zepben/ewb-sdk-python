3df68b2289d74469f4d1f781aff8b919734c8d49
Vincent White <vince.white@zepben.com>
2023-09-06 17:35:03 +1000

    Added transformer cooling types and regulating control model kinds
    Signed-off-by: Vincent White <vince.white@zepben.com>

---
+++ src/zepben/evolve/model/cim/iec61970/base/wires/regulating_control_model_kind.py
@@ -0,0 +1,43 @@
+#  Copyright 2023 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from enum import Enum
+
+__all__ = ["RegulatingControlModeKind"]
+
+
+class RegulatingControlModeKind(Enum):
+    """The kind of regulation model. For example regulating voltage, reactive power, active power, etc."""
+
+    UNKNOWN_CONTROL_MODE = 0
+    """Default, unknown."""
+
+    voltage = 1
+    """Voltage is specified."""
+
+    activePower = 2
+    """Active power is specified."""
+
+    reactivePower = 3
+    """Reactive power is specified."""
+
+    currentFlow = 4
+    """Current flow is specified."""
+
+    admittance = 5
+    """Admittance is specified."""
+
+    timeScheduled = 6
+    """Control switches on/off by time of day. The times may change on the weekend, or in different seasons."""
+
+    temperature = 7
+    """Control switches on/off based on the local temperature (i.e., a thermostat)."""
+
+    powerFactor = 8
+    """Power factor is specified."""
+
+    @property
+    def short_name(self):
+        return str(self)[26:]
---
+++ src/zepben/evolve/model/cim/iec61970/base/wires/transformer_cooling_type.py
@@ -0,0 +1,49 @@
+#  Copyright 2023 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from enum import Enum
+
+__all__ = ["TransformerCoolingType"]
+
+
+class TransformerCoolingType(Enum):
+    """Transformer cooling types."""
+
+    UNKNOWN_COOLING_TYPE = 0
+    """Default"""
+
+    ONAN = 1
+    """Oil natural, air natural"""
+
+    ONAF = 2
+    """Oil natural, air forced"""
+
+    OFAF = 3
+    """Oil forced, air forced"""
+
+    OFWF = 4
+    """Oil forced, water forced"""
+
+    ODAF = 5
+    """Oil directed, air forced"""
+
+    KNAN = 6
+    """Non-mineral oil natural, air natural"""
+
+    KNAF = 7
+    """Non-mineral oil natural, air forced"""
+
+    KFAF = 8
+    """Non-mineral oil forced, air forced"""
+
+    KFWF = 9
+    """Non-mineral oil forced, water forced"""
+
+    KDAF = 10
+    """ Non-mineral oil directed, air forced"""
+
+    @property
+    def short_name(self):
+        return str(self)[23:]
