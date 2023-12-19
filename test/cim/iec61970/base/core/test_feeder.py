d0a859435457a1adcd04fb51099946fe8b328ce2
vincewhite <33650891+vincewhite@users.noreply.github.com>
2023-12-19 14:26:59 +1100

    EWB-4143 Update Assigning Feeder Head Terminal to Match JVM SDK (#142)
    Signed-off-by: Vince White <vince.white@zepben.com>

--- test/cim/iec61970/base/core/test_feeder.py
+++ test/cim/iec61970/base/core/test_feeder.py
@@ -3,13 +3,14 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from pytest import raises
 from hypothesis import given
 from hypothesis.strategies import builds, lists
 
 from cim.collection_validator import validate_collection_unordered
 from cim.iec61970.base.core.test_equipment_container import equipment_container_kwargs, verify_equipment_container_constructor_default, \
     verify_equipment_container_constructor_kwargs, verify_equipment_container_constructor_args, equipment_container_args
-from zepben.evolve import Feeder, Terminal, Substation, Equipment, LvFeeder
+from zepben.evolve import Feeder, Terminal, Substation, Equipment, LvFeeder, Switch
 
 feeder_kwargs = {
     **equipment_container_kwargs,
@@ -77,3 +78,69 @@ def test_normal_energized_lv_feeder_collection():
                                   Feeder.remove_normal_energized_lv_feeder,
                                   Feeder.clear_normal_energized_lv_feeders,
                                   KeyError)
+
+
+def test_can_update_normal_head_terminal_on_empty_feeder():
+    empty_feeder = Feeder()
+    terminal1 = Terminal()
+    terminal2 = Terminal()
+
+    empty_feeder.normal_head_terminal = terminal1
+    assert empty_feeder.normal_head_terminal == terminal1
+
+    empty_feeder.normal_head_terminal = terminal2
+    assert empty_feeder.normal_head_terminal == terminal2
+
+    empty_feeder.normal_head_terminal = None
+    assert empty_feeder.normal_head_terminal is None
+
+    equipment = Switch()
+    empty_feeder.add_equipment(equipment).add_current_equipment(equipment)
+    empty_feeder.clear_equipment().clear_current_equipment()
+
+    empty_feeder.normal_head_terminal = terminal1
+    assert empty_feeder.normal_head_terminal == terminal1
+
+
+def test_block_normal_head_terminal_update_when_equipment_assigned():
+    feeder = Feeder(mrid="fdr")
+    terminal1 = Terminal()
+    terminal2 = Terminal()
+
+    equipment = Switch()
+
+    feeder.add_equipment(equipment)
+
+    # allows initial assignment
+    feeder.normal_head_terminal = terminal1
+    assert feeder.normal_head_terminal == terminal1
+
+    # doesn't raise exception on trying to reassign the same terminal
+    feeder.normal_head_terminal = terminal1
+    assert feeder.normal_head_terminal == terminal1
+
+    with raises(ValueError, match="Feeder fdr has equipment assigned to it. Cannot update normalHeadTerminal on a feeder with equipment assigned."):
+        feeder.normal_head_terminal = terminal2
+    assert feeder.normal_head_terminal == terminal1
+
+
+def test_block_normal_head_terminal_update_when_current_equipment_assigned():
+    feeder = Feeder(mrid="fdr")
+    terminal1 = Terminal()
+    terminal2 = Terminal()
+
+    equipment = Switch()
+
+    feeder.add_current_equipment(equipment)
+
+    # allows initial assignment
+    feeder.normal_head_terminal = terminal1
+    assert feeder.normal_head_terminal == terminal1
+
+    # doesn't raise exception on trying to reassign the same terminal
+    feeder.normal_head_terminal = terminal1
+    assert feeder.normal_head_terminal == terminal1
+
+    with raises(ValueError, match="Feeder fdr has equipment assigned to it. Cannot update normalHeadTerminal on a feeder with equipment assigned."):
+        feeder.normal_head_terminal = terminal2
+    assert feeder.normal_head_terminal == terminal1
