f15cdf4d127c43c556f8e7417d9636e3a8e9c264
vince <vince.white@zepben.com>
2024-05-06 16:22:41 +1000

    hold
    Signed-off-by: vince <vince.white@zepben.com>

--- src/zepben/evolve/database/paths/ewb_data_file_paths.py
+++ src/zepben/evolve/database/paths/ewb_data_file_paths.py
@@ -3,7 +3,7 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
-from datetime import date
+from datetime import date, timedelta
 from pathlib import Path
 from typing import Callable, Iterator, Optional, List
 
@@ -14,143 +14,146 @@ __all__ = ['EwbDataFilePaths']
 
 
 class EwbDataFilePaths:
-    baseDir: Path = None
-    createDirectories: Callable[[Path], Path] = None
-    isDirectory: Callable[[Path], bool] = None
+    _base_dir: Path = None
+    create_directories: Callable[[Path], Path] = None
+    is_directory: Callable[[Path], bool] = None
     exists: Callable[[Path], bool] = None
-    listsFiles: Callable[[Path], Iterator[Path]] = None
+    list_files: Callable[[Path], Iterator[Path]] = None
 
-    def __init__(self, baseDir: Path = Path("/tmp/"),
-                 createPath: bool = False,
-                 createDirectories: Callable[[Path], Path] = lambda it: it.mkdir(parents=True),
-                 isDirectory: Callable[[Path], bool] = Path.is_dir,
+    def __init__(self, base_dir: Path,
+                 create_path: bool = False,
+                 create_directories: Callable[[Path], Path] = lambda it: it.mkdir(parents=True),
+                 is_directory: Callable[[Path], bool] = Path.is_dir,
                  exists: Callable[[Path], bool] = Path.exists,
-                 listsFiles: Callable[[Path], Iterator[Path]] = Path.iterdir):
+                 list_files: Callable[[Path], Iterator[Path]] = Path.iterdir):
 
-        if createDirectories is not None:
-            self.createDirectories = createDirectories
-        if isDirectory is not None:
-            self.isDirectory = isDirectory
+        if create_directories is not None:
+            self.create_directories = create_directories
+        if is_directory is not None:
+            self.is_directory = is_directory
         if exists is not None:
             self.exists = exists
-        if listsFiles is not None:
-            self.listsFiles = listsFiles
-        if baseDir is not None:
-            self.baseDir = baseDir
+        if list_files is not None:
+            self.list_files = list_files
+        if base_dir is not None:
+            self._base_dir = base_dir
 
-        if createPath:
-            self.createDirectories(baseDir)
+        if create_path:
+            self.create_directories(base_dir)
 
-        require(self.isDirectory(baseDir), lambda: f"baseDir must be a directory")
+        require(self.is_directory(base_dir), lambda: f"baseDir must be a directory")
 
+    @property
+    def baseDir(self):
+        return self._base_dir
 
+    def customer(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.CUSTOMER.fileDescriptor)
 
-    def customer(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.CUSTOMER.fileDescriptor)
+    def diagram(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.DIAGRAM.fileDescriptor)
 
-    def diagram(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.DIAGRAM.fileDescriptor)
+    def measurement(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.MEASUREMENT.fileDescriptor)
 
-    def measurement(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.MEASUREMENT.fileDescriptor)
+    def networkModel(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.NETWORK_MODEL.fileDescriptor)
 
-    def networkModel(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.NETWORK_MODEL.fileDescriptor)
+    def tileCache(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.TILE_CACHE.fileDescriptor)
 
-    def tileCache(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.TILE_CACHE.fileDescriptor)
+    def energyReading(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.ENERGY_READING.fileDescriptor)
 
-    def energyReading(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.ENERGY_READING.fileDescriptor)
+    def energyReadingsIndex(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.ENERGY_READINGS_INDEX.fileDescriptor)
 
-    def energyReadingsIndex(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.ENERGY_READINGS_INDEX.fileDescriptor)
+    def loadAggregatorMetersByDate(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.LOAD_AGGREGATOR_METERS_BY_DATE.fileDescriptor)
 
-    def loadAggregatorMetersByDate(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.LOAD_AGGREGATOR_METERS_BY_DATE.fileDescriptor)
+    def weatherReading(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.WEATHER_READING.fileDescriptor)
 
-    def weatherReading(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.WEATHER_READING.fileDescriptor)
+    def resultsCache(self, model_date: date) -> Path:
+        return self._to_dated_path(model_date, DatabaseType.RESULTS_CACHE.fileDescriptor)
 
-    def resultsCache(self, date: datetime.date) -> Path:
-        return self._to_dated_path(date, DatabaseType.RESULTS_CACHE.fileDescriptor)
-
-    def createDirectories2(self, date: datetime.date) -> Path:
-        datePath = Path(str(self.baseDir), str(date))
-        if self.exists(datePath):
-            return datePath
+    def createDirectories(self, model_date: date) -> Path:
+        date_path = Path(str(self._base_dir), str(model_date))
+        if self.exists(date_path):
+            return date_path
         else:
-            return self.createDirectories(datePath)
+            return self.create_directories(date_path)
 
-    def _to_dated_path(self, date: datetime.date, file: str) -> Path:
-        return Path(str(self.baseDir), str(date), f"{date}-{file}.sqlite")
+    def _to_dated_path(self, model_date: date, file: str) -> Path:
+        return Path(str(self._base_dir), str(model_date), f"{model_date}-{file}.sqlite")
 
-    def check_exists(self, type: DatabaseType, date: datetime.date) -> bool:
-        if not type.perDate:
+    def check_exists(self, database_type: DatabaseType, model_date: date) -> bool:
+        if not database_type.perDate:
             raise ValueError("INTERNAL ERROR: Should only be calling `checkExists` for `perDate` files.")
 
-        if type == DatabaseType.CUSTOMER:
-            model_path = self.customer(date)
-        elif type == DatabaseType.DIAGRAM:
-            model_path = self.diagram(date)
-        elif type == DatabaseType.MEASUREMENT:
-            model_path = self.measurement(date)
-        elif type == DatabaseType.NETWORK_MODEL:
-            model_path = self.networkModel(date)
-        elif type == DatabaseType.TILE_CACHE:
-            model_path = self.tileCache(date)
-        elif type == DatabaseType.ENERGY_READING:
-            model_path = self.energyReading(date)
+        if database_type == DatabaseType.CUSTOMER:
+            model_path = self.customer(model_date)
+        elif database_type == DatabaseType.DIAGRAM:
+            model_path = self.diagram(model_date)
+        elif database_type == DatabaseType.MEASUREMENT:
+            model_path = self.measurement(model_date)
+        elif database_type == DatabaseType.NETWORK_MODEL:
+            model_path = self.networkModel(model_date)
+        elif database_type == DatabaseType.TILE_CACHE:
+            model_path = self.tileCache(model_date)
+        elif database_type == DatabaseType.ENERGY_READING:
+            model_path = self.energyReading(model_date)
         else:
             raise ValueError(
                 "INTERNAL ERROR: Should only be calling `checkExists` for `perDate` files, which should all be covered above, so go ahead and add it.")
         return self.exists(model_path)
 
-    def findClosest(self, type: DatabaseType, maxDaysToSearch: int = 999999, date: datetime.date = datetime.date.today(), searchForwards: bool = False) -> Optional[datetime.date]:
-        if not type.perDate:
+    def findClosest(self, database_type: DatabaseType, max_days_to_search: int = 999999, model_date: date = date.today(), search_forwards: bool = False) -> \
+    Optional[date]:
+        if not database_type.perDate:
             return None
 
-        if self.check_exists(type, date):
-            return date
+        if self.check_exists(database_type, model_date):
+            return model_date
 
         offset = 1
 
-        while offset <= maxDaysToSearch:
-            offset_days = datetime.timedelta(offset)
+        while offset <= max_days_to_search:
+            offset_days = timedelta(offset)
             try:
-                previous_date = date - offset_days
-                if self.check_exists(type, previous_date):
+                previous_date = model_date - offset_days
+                if self.check_exists(database_type, previous_date):
                     return previous_date
-            except OverflowError as e:
+            except OverflowError:
                 pass
 
-            if searchForwards:
+            if search_forwards:
                 try:
-                    forward_date = date + offset_days
-                    if self.check_exists(type, forward_date):
+                    forward_date = model_date + offset_days
+                    if self.check_exists(database_type, forward_date):
                         return forward_date
-                except OverflowError as e:
+                except OverflowError:
                     pass
             offset += 1
         return None
 
-    def _getAvailableDatesFor(self, type: DatabaseType) -> List[datetime.date]:
-        if not type.perDate:
+    def _getAvailableDatesFor(self, database_type: DatabaseType) -> List[date]:
+        if not database_type.perDate:
             raise ValueError(
                 "INTERNAL ERROR: Should only be calling `getAvailableDatesFor` for `perDate` files.")
 
         to_return = list()
 
-        for file in self.listsFiles(self.baseDir):
-            if self.isDirectory(file):
+        for file in self.list_files(self._base_dir):
+            if self.is_directory(file):
                 try:
-                    date = datetime.date.fromisoformat(file.name)
-                    if self.exists(self._to_dated_path(date, type.fileDescriptor)):
-                        to_return.append(date)
+                    model_date = date.fromisoformat(file.name)
+                    if self.exists(self._to_dated_path(model_date, database_type.fileDescriptor)):
+                        to_return.append(model_date)
                 except ValueError:
                     pass
         to_return.sort()
         return to_return
 
-    def getNetworkModelDatabases(self) -> List[datetime.date]:
+    def getNetworkModelDatabases(self) -> List[date]:
         return self._getAvailableDatesFor(DatabaseType.NETWORK_MODEL)
