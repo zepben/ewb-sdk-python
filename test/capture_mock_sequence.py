5948889911c9dd18a6f109413a6bcabb35d46e2d
Max Chesterfield <max.chesterfield@zepben.com>
2025-03-31 18:41:30 +1100

    extra reporting for test failures in obscure places

--- test/capture_mock_sequence.py
+++ test/capture_mock_sequence.py
@@ -4,6 +4,8 @@
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from unittest.mock import Mock
 
+import pytest
+
 
 class CaptureMockSequence:
 
@@ -16,4 +18,24 @@ class CaptureMockSequence:
             self.sequence.attach_mock(mock, key)
 
     def verify_sequence(self, expected_calls):
-        assert self.sequence.mock_calls == expected_calls, "mismatch in actual vs expected calls"
+        mock_calls = list(self.sequence.mock_calls)
+
+        mock_call_len = len(mock_calls)
+        if mock_call_len != len(expected_calls):
+            print(f'call sequence lengths not the same\n\n +++++++++++ \n\n')
+            if mock_call_len > len(expected_calls):
+                enum_list = mock_calls
+                cmp_list = expected_calls
+            else:
+                enum_list = expected_calls
+                cmp_list = mock_calls
+        else:
+            enum_list = mock_calls
+            cmp_list = expected_calls
+
+        for i, call in enumerate(enum_list):
+            if i < len(cmp_list):
+                print(f'{call} => {cmp_list[i]}')
+            else:
+                print(f'{call}')
+        assert mock_calls == expected_calls, "mismatch in actual vs expected calls"
