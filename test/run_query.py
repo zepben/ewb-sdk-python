2917d640edaa5a207f9093963ec8b5cc4dc263c3
Anthony Charlton <anthony.charlton@zepben.com>
2025-06-13 11:34:22 +1000

    WIP
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- test/run_query.py
+++ test/run_query.py
@@ -7,7 +7,7 @@ import platform
 import shlex
 from typing import List, Callable, Any
 
-from zepben.evolve import connect_insecure, NetworkService, IdentifiedObject
+from zepben.evolve import connect_insecure, NetworkService, IdentifiedObject, Tracing, downstream, upstream, stop_at_open, NetworkTrace
 from zepben.evolve.streaming.get.network_consumer import NetworkConsumerClient
 
 rpc_port = 9100
@@ -22,6 +22,10 @@ async def run_queries():
         await run_query(client, "with FNS022 select where is powertransformer")
         await run_query(client, "with FNS022 select where name like A")
         await run_query(client, "with FNS022 select where length < 10")
+
+        await run_query(client, "with FNS022 trace from 19404384")
+        await run_query(client, "with FNS022 trace downstream from 19404384")
+        await run_query(client, "with FNS022 trace upstream from 19404384")
 
 
 async def run_query(client: NetworkConsumerClient, query: str):
@@ -57,19 +61,33 @@ class EquipmentContainerCommand:
 
         sub_command = self.additional_args.pop(0).lower()
         if sub_command == "select":
-            where_check = self.additional_args.pop(0).lower()
-            assert where_check == "where", "missing keyword 'WHERE' after 'SELECT'"
             await SelectCommand(self.additional_args).run(client.service)
+        elif sub_command == "trace":
+            await TraceCommand(self.additional_args).run(client.service)
         else:
             raise ValueError(f"unknown sub-command '{sub_command}'")
 
 
-class SelectCommand:
+class BaseCommand:
+
+    @staticmethod
+    def _check_attr(io: IdentifiedObject, attr: str, compare: Callable[[Any], bool]) -> bool:
+        try:
+            return compare(getattr(io, attr))
+        except:
+            return False
+
+
+class SelectCommand(BaseCommand):
 
     def __init__(self, additional_args: List[str]):
+        super().__init__()
         self.additional_args = additional_args
 
     async def run(self, service: NetworkService):
+        where_check = self.additional_args.pop(0).lower()
+        assert where_check == "where", "missing keyword 'WHERE' after 'SELECT'"
+
         condition_check = self.additional_args.pop(0)
 
         if condition_check.lower() == "is":
@@ -104,12 +122,40 @@ class SelectCommand:
             remaining_condition = " ".join(self.additional_args)
             raise ValueError(f"unexpected remaining tokens: '{remaining_condition}'")
 
-    @staticmethod
-    def _check_attr(io: IdentifiedObject, attr: str, compare: Callable[[Any], bool]) -> bool:
-        try:
-            return compare(getattr(io, attr))
-        except:
-            return False
+
+class TraceCommand(BaseCommand):
+
+    def __init__(self, additional_args: List[str]):
+        super().__init__()
+        self.additional_args = additional_args
+
+    async def run(self, service: NetworkService):
+        trace_type = self.additional_args.pop(0).lower()
+
+        traced = []
+        trace: NetworkTrace[None] = \
+            Tracing.network_trace() \
+                .add_condition(stop_at_open()) \
+                .add_step_action(lambda step, context: traced.append(step.path.to_equipment))
+
+        if trace_type == "downstream":
+            from_check = self.additional_args.pop(0).lower()
+            assert from_check == "from", "missing keyword 'FROM' after 'DOWNSTREAM'"
+            trace.add_condition(downstream())
+        elif trace_type == "upstream":
+            from_check = self.additional_args.pop(0).lower()
+            assert from_check == "from", "missing keyword 'FROM' after 'UPSTREAM'"
+            trace.add_condition(upstream())
+        elif trace_type != "from":
+            raise ValueError(f"unknown trace '{trace_type}'")
+
+        start_point = self.additional_args.pop(0)
+        await trace.run(service[start_point])
+        print([it.mrid for it in traced])
+
+        if self.additional_args:
+            remaining_condition = " ".join(self.additional_args)
+            raise ValueError(f"unexpected remaining tokens: '{remaining_condition}'")
 
 
 if __name__ == "__main__":
