a2658b00edfd932518cf2b55c81d2016e8db9d4b
Max Chesterfield <max.chesterfield@zepben.com>
2025-08-04 15:15:10 +1000

    leaves now calculated when specified
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- src/zepben/evolve/services/network/tracing/networktrace/actions/tree_node.py
+++ src/zepben/evolve/services/network/tracing/networktrace/actions/tree_node.py
@@ -3,7 +3,7 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from typing import List, TypeVar, Generic
+from typing import Set, TypeVar, Generic
 
 from zepben.evolve import IdentifiedObject
 
