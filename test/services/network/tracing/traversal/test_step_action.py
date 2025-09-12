08b1fe3ecf6fc206d023ecbb55760cd8de2f721e
Max Chesterfield <max.chesterfield@zepben.com>
2025-09-12 12:42:36 +1000

    not, not today
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- test/services/network/tracing/traversal/test_step_action.py
+++ test/services/network/tracing/traversal/test_step_action.py
@@ -2,6 +2,7 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import pytest
 from pytest import raises
 
 from zepben.ewb import StepAction, StepContext
@@ -49,6 +50,7 @@ class TestStepAction:
 
         assert captured == [(expected_item, expected_ctx)]
 
+    @pytest.mark.asyncio
     async def test_async_step_action(self):
         captured = []
 
