10d9f427677df8fa0ff3a034272b5864b6450245
charlta <7707606+charlta@users.noreply.github.com>
2026-06-19 15:49:26 +1000

    [DEV-9273] code tidy (#228)
    * moved `__all__` blocks to the top of all files
    * fixed EOF blank lines
    
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- src/zepben/ewb/model/cim/extensions/iec61970/base/feeder/lv_substation.py
+++ src/zepben/ewb/model/cim/extensions/iec61970/base/feeder/lv_substation.py
@@ -2,9 +2,10 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
-from typing import Generator, TYPE_CHECKING
 
 __all__ = ['LvSubstation']
+
+from typing import Generator, TYPE_CHECKING
 
 from zepben.ewb import ngen, nlen, safe_remove_by_id, get_by_mrid
 from zepben.ewb.model.cim.extensions.zbex import zbex
