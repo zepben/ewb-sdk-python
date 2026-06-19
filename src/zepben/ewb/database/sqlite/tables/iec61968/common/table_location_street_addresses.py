b58784358415aa739e3a48897017d8b40ae3af4e
Anthony Charlton <anthony.charlton@zepben.com>
2026-06-19 14:28:56 +1000

    moved `__all__` blocks to the top of all files
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- src/zepben/ewb/database/sqlite/tables/iec61968/common/table_location_street_addresses.py
+++ src/zepben/ewb/database/sqlite/tables/iec61968/common/table_location_street_addresses.py
@@ -3,12 +3,12 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+__all__ = ["TableLocationStreetAddresses"]
+
 from typing import List, Generator
 
 from zepben.ewb.database.sql.column import Column, Nullable
 from zepben.ewb.database.sqlite.tables.iec61968.common.table_street_addresses import TableStreetAddresses
-
-__all__ = ["TableLocationStreetAddresses"]
 
 
 class TableLocationStreetAddresses(TableStreetAddresses):
