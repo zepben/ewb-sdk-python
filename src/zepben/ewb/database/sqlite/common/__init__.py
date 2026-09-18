42f13f0f27626848ee84c29c26fd6e2de9c66c89
Max Chesterfield <max.chesterfield@zepben.com>
2026-09-18 11:14:03 +1000

    python 3.10 compat
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- src/zepben/ewb/database/sqlite/common/__init__.py
+++ src/zepben/ewb/database/sqlite/common/__init__.py
@@ -10,9 +10,10 @@ if TYPE_CHECKING:
     from zepben.ewb.database.sqlite.network.network_cim_writer import TSqlTable
 
 
-def using_table(table: Type[TSqlTable]):
+# TODO: after 3.10 support is dropped, the quoted typevars below can be unquoted.
+def using_table(table: "Type[TSqlTable]"):
     def wrapper(func):
-        def _inner(self, io: Identifiable, *args, **kwargs):
+        def _inner(self, io: "Identifiable", *args, **kwargs):
             _table: TSqlTable = self._database_tables.get_table(table)
             _insert = self._database_tables.get_insert(table)
             return func(self, io, *args, table=_table, insert=_insert, **kwargs)
